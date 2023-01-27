c708feb2b45d80c40b1eb89669fb893b0ad5b124
Chad <chadhly@yahoo.com>
2023-01-27 14:54:06 -0500

    Second commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import math
+def parallel(resistor_list):
+    Total_Resistance=0
+    for resistor in resistor_list:
+        Total_Resistance= Total_Resistance + 1/resistor
+        
+    Total_Resistance= 1/Total_Resistance
+
+    print(Total_Resistance, "ohms")
+    return Total_Resistance
+    
+ 
+def potential_divider(volts, voltage_list): 
+    Total_voltage= 0
+    for voltage in voltage_list:
+        Total_voltage=Total_voltage + voltage
+    
+    i= volts/ Total_voltage
+    
+    for voltage in voltage_list:
+        v = voltage * i
+    
+    print(v, "Total_voltage")
+    
+        
+    
+parallel([1,67])
+potential_divider(9,[1, 3])    
